9a9d73b388a8b2aab6bb82a63468cf669883572f
Hanumanth Reddy <putluru.hanumanth@nendrasys.com>
2016-08-12 15:59:45 +0530

    Code formatting in Exceptions.

--- src/com/mmt/exceptions/EndpointNotExistsException.java
+++ src/com/mmt/exceptions/EndpointNotExistsException.java
@@ -1,26 +1,21 @@
 package com.mmt.exceptions;
 
-public class EndpointNotExistsException extends Throwable
-{
-
+@SuppressWarnings("serial")
+public class EndpointNotExistsException extends Throwable {
 	public EndpointNotExistsException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public EndpointNotExistsException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public EndpointNotExistsException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public EndpointNotExistsException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
-	
+
 }
